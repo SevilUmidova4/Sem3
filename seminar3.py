c27c438e91c2342c8c1d9540a3eb2e32797c7a1b
Sevil <sevil.umidova.su@gmail.com>
2023-09-28 12:34:40 +0500

    this is my conversion

---
+++ seminar3.py
@@ -0,0 +1,10 @@
+#task5
+decimalNumber = input("Please, type decimal \n")
+decimaltoBinary = bin(int(decimalNumber))
+print("This is binary", decimaltoBinary)
+
+decimaltoOct = oct(int(decimalNumber))
+print("This is oct", decimaltoOct)
+
+decimaltoHex = hex(int(decimalNumber))
+print("This is hex", decimaltoHex)
